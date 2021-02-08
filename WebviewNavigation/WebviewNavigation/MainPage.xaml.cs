0bda711294f8b490135f39564fae2674654bbb26
Victor <victor@sommaior.com.br>
2021-02-08 14:34:47 -0300

    Added: Webview with problems project

---
+++ WebviewNavigation/WebviewNavigation/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebviewNavigation.ViewModels;
+using Xamarin.Forms;
+
+namespace WebviewNavigation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void WebView_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            if (BindingContext is MainViewModel vm)
+            {
+                vm.Navigated(Dispatcher);
+            }
+        }
+    }
+}
---
+++ WebviewNavigation/WebviewNavigation/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WebviewNavigation.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private bool _showWebView;
+
+        public bool ShowWebView
+        {
+            get { return _showWebView; }
+            set
+            {
+                if (_showWebView != value)
+                {
+                    _showWebView = value;
+                    NotifyPropertyChanged(); 
+                }
+            }
+        }
+
+        private bool _showLoading = true;
+
+        public bool ShowLoading
+        {
+            get { return _showLoading; }
+            set
+            {
+                if (_showLoading != value)
+                {
+                    _showLoading = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+
+        internal void Navigated(IDispatcher dispatcher)
+        {
+            dispatcher.BeginInvokeOnMainThread(() =>
+            {
+                ShowWebView = true;
+                ShowLoading = false;
+            });
+        }
+    }
+}
